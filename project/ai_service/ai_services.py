bac002102fda79c68ebc7b69962191b0dfe2aedc
qliu4@bsu.edu <github675lqw>
2019-10-02 12:31:15 -0400

    Add /upload

--- project/ai_service/ai_services.py
+++ project/ai_service/ai_services.py
@@ -1,4 +1,31 @@
 from flask import jsonify
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-def run_linear_regression():
+
+def allowed_file(filename, allowed_extentions):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_extentions
+
+def upload(file=None):
+    ALLOWED_EXTENSIONS = {'csv'}
+    if allowed_file(file.filename, ALLOWED_EXTENSIONS):
+        file_name = file.filename
+
+
+        return jsonify({'file_name': file_name}), 200
+    else:
+        return jsonify({'error_message': 'Wrong file format'}), 400
+
+def run_linear_regression(X):
+    """
+    :param:
+    :return:
+    """
+    # y = 1 * x_0 + 2 * x_1 + 3
+    y = np.dot(X, np.array([1, 2])) + 3
+    reg = LinearRegression().fit(X, y)
     return jsonify({"output": 'success'})
+
+def run_pca():
+    return jsonify({"output": 'run_pca_success'})
--- project/tests/test_factory.py
+++ project/tests/test_factory.py
@@ -1,14 +1,24 @@
 from ai_service.server import create_app
+from flask import jsonify
 
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
+def test_upload():
+    pass
 
 def test_hello(client):
     response = client.get('/hello')
+    print(type(response))
     assert response.data == b'Hello, World!'
 
 def test_linear_regression(client):
     response = client.get('/cloudmesh-ai-services/linear-regression')
     assert response.data == b'{"output":"success"}\n'
+
+
+def test_run_pca(client):
+    response = client.get('/cloudmesh-ai-services/pca')
+
+    assert response.data == ''
