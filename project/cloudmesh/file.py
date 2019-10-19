d93b119d32fa963b2fa4b5d44e0458eea3bf54d4
Gregor von Laszewski <laszewski@gmail.com>
2019-10-19 02:29:27 -0400

    Create file.py
    separate files from ai service

---
+++ project/cloudmesh/file.py
@@ -0,0 +1,39 @@
+import os
+from flask import jsonify, current_app
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from werkzeug.utils import secure_filename
+import pandas as pd
+import pickle
+
+def read(file_name):
+    try:
+        data = private_read_file(file_name)
+    except:
+        return jsonify({'error_message': 'failed to read'})
+    return jsonify({file_name: data.tolist()})
+
+def read_csv(file_name):
+    data = pd.read_csv(os.path.join(current_app.config['UPLOAD_FOLDER'], file_name + '.csv', ), header=None)
+    return data.values
+
+def allowed(file_name, allowed_extentions):
+    return '.' in file_name and \
+           file_name.rsplit('.', 1)[1].lower() in allowed_extentions
+
+def save(file):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+    # TODO: if save failed?
+
+def upload(file=None):
+    ALLOWED_EXTENSIONS = {'csv'}
+    if file and allowed_file(file.filename, ALLOWED_EXTENSIONS):
+        save_file(file)
+        return jsonify({'file_name': file.filename}), 200
+    else:
+        return jsonify({'error_message': 'Wrong file format'}), 400
+
+def list():
+    files = os.listdir(current_app.config['UPLOAD_FOLDER'])
+    return jsonify({'files': files})
