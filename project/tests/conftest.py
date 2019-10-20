7c42eba55c467f49694f2a3beee70b30732a9cb4
Qiwei Liu qiwliu@iu.edu <github675lqw>
2019-10-20 10:52:19 -0400

    Done prototyping linear regression

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -10,7 +10,7 @@ from db import get_db, init_db
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
-
+        
     app = create_app({
         'TESTING': True,
         'DATABASE': db_path,
